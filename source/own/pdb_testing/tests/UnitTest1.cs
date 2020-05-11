1e81fe3ea851ed641b25928f2df75ae0e9511074
Willem van Ketwich <willvk@gmail.com>
2020-05-11 19:05:22 +1000

    add some tests

--- source/own/pdb_testing/tests/UnitTest1.cs
+++ source/own/pdb_testing/tests/UnitTest1.cs
@@ -1,4 +1,7 @@
 using System;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
 using Xunit;
 
 namespace pdb_testing
@@ -6,9 +9,45 @@ namespace pdb_testing
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Test_CanGetMetaDataFromDll()
         {
+            string peFilePath = Path.GetFullPath("../../../../../artefacts/test_debug.dll");
+            var file = File.OpenRead(peFilePath);
+            var peReader = new PEReader(file);
+            var metaDataReader = peReader.GetMetadataReader();
 
+            Assert.NotNull(metaDataReader);
+        }
+
+        [Fact]
+        public void Test_GetMetaDataFromPdbThrowsException()
+        {
+            string peFilePath = Path.GetFullPath("../../../../../artefacts/test_debug.pdb");
+            var file = File.OpenRead(peFilePath);
+            var peReader = new PEReader(file);
+
+            Assert.Throws<BadImageFormatException>(() => peReader.GetMetadataReader());
+        }
+
+        [Fact]
+        public void Test_CanGetMetaDataProviderFromStreamForPdb()
+        {
+            string peFilePath = Path.GetFullPath("../../../../../artefacts/test_debug.pdb");
+            var file = File.OpenRead(peFilePath);
+            var mdProvider = MetadataReaderProvider.FromMetadataStream(file);
+
+            Assert.NotNull(mdProvider);
+        }
+
+        [Fact]
+        public void Test_CagGetMetaDataProviderFromStreamForPdb()
+        {
+            string peFilePath = Path.GetFullPath("../../../../../artefacts/test_debug.pdb");
+            var file = File.OpenRead(peFilePath);
+            var mdProvider = MetadataReaderProvider.FromMetadataStream(file);
+            var mdReader = mdProvider.GetMetadataReader();
+
+            Assert.NotNull(mdReader);
         }
     }
 }
