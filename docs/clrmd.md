43d611dfa6335313d5ecfbcd267eb53046a31e89
Willem van Ketwich <willvk@gmail.com>
2020-05-17 11:25:22 +1000

    add start of debugging with clrmd. add to docs

--- docs/clrmd.md
+++ docs/clrmd.md
@@ -6,3 +6,9 @@ https://github.com/microsoft/clrmd/blob/master/doc/GettingStarted.md
 invasive, non-invasive and passive attach to process modes:
 
 To be clear though, the difference between an invasive and non-invasive attach doesn't matter to CLRMD. It only matters if you need to control the process through the IDebug interfaces. If you do not care about getting debugger events or breaking/continuing the process, you should choose a non-invasive attach.
+
+useful example of determining the source line of a break and the IL:
+https://github.com/microsoft/clrmd/blob/master/src/Samples/FileAndLineNumbers/Program.cs
+
+attaching to a live process:
+https://github.com/microsoft/clrmd/blob/master/doc/GettingStarted.md#attaching-to-a-live-process
--- source/own/clrmd_testing/debug/Program.cs
+++ source/own/clrmd_testing/debug/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Diagnostics.Runtime;
 
 namespace debug
 {
@@ -6,7 +7,21 @@ namespace debug
     {
         static void Main(string[] args)
         {
+            // needs to run in docker: AttachToProcess is not supported on Darwin 19.4.0
+            int.TryParse(args[0], out var processId);
+            Console.WriteLine("Attaching for PID: " + processId.ToString());
+            var suspendOnStart = false;
             Console.WriteLine("Hello World!");
+            DataTarget dataTarget = DataTarget.AttachToProcess(processId, suspendOnStart);
+
+            if(dataTarget != null)
+            {
+              Console.WriteLine("Attached.");
+            }
+            else
+            {
+              Console.WriteLine("Not attached.");
+            }
         }
     }
 }
