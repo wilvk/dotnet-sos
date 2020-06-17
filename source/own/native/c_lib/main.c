f8d277335e5aabf759609fb3469ccd913f401782
Willem van Ketwich <willvk@gmail.com>
2020-06-17 21:47:24 +1000

    formatting

--- source/own/native/c_lib/main.c
+++ source/own/native/c_lib/main.c
@@ -6,23 +6,17 @@ int main()
   void ReturnNothing();
 
   const char * returnStringString = ReturnString();
-
   printf("%s\n", returnStringString);
 
   char * originalString = "OriginalString";
-
   const char * takeAndReturnNewStringString = TakeAndReturnNewString(originalString);
-
   printf("%s\n", takeAndReturnNewStringString);
 
   int returnIntInt = ReturnInt();
-
   printf("%d\n", returnIntInt);
 
   int originalInt = 8;
-
   int takeOneIntAndReturnNewIntInt = TakeOneIntAndReturnNewInt(originalInt);
-
   printf("%d\n", takeOneIntAndReturnNewIntInt);
 
   return 0;
--- source/own/native/cs_lib/main.cpp
+++ source/own/native/cs_lib/main.cpp
@@ -8,23 +8,17 @@ int main()
   void ReturnNothing();
 
   const char * returnStringString = ReturnString();
-
   printf("%s\n", returnStringString);
 
   const char * originalString = "OriginalString";
-
   const char * takeAndReturnNewStringString = TakeAndReturnNewString(originalString);
-
   printf("%s\n", takeAndReturnNewStringString);
 
   int returnIntInt = ReturnInt();
-
   printf("%d\n", returnIntInt);
 
   int originalInt = 8;
-
   int takeOneIntAndReturnNewIntInt = TakeOneIntAndReturnNewInt(originalInt);
-
   printf("%d\n", takeOneIntAndReturnNewIntInt);
 
   return 0;
