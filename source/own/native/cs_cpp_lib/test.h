ff120738e7366534108b09f2f1e8b486a19c31bd
Willem van Ketwich <willvk@gmail.com>
2020-06-19 18:39:19 +1000

    headers not required for cs

--- source/own/native/cs_cpp_lib/test.h
+++
@@ -1,9 +0,0 @@
-extern void DoNothing();
-
-extern const char * ReturnString();
-
-extern const char * TakeAndReturnNewString(const char * OriginalString);
-
-extern int ReturnInt();
-
-extern int TakeOneIntAndReturnNewInt(int OriginalInt);
