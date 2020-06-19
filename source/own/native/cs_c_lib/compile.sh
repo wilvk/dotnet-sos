b2260eed7c9413f9e6e00d1cc928a58b750004a3
Willem van Ketwich <willvk@gmail.com>
2020-06-19 18:40:23 +1000

    remove echos

--- source/own/native/cs_c_lib/compile.sh
+++ source/own/native/cs_c_lib/compile.sh
@@ -5,5 +5,3 @@ rm libtest.dylib &2> /dev/null
 gcc -g -c test.c -o test.o                                                      # compiling a C library
 gcc -dynamiclib -undefined suppress -flat_namespace test.o -o libtest.dylib     # generate a dynamic library from our object file
 dotnet build                                                                    # compiling our C# application
-
-echo "to run, enter: dotnet run"
--- source/own/native/cs_cpp_lib/compile.sh
+++ source/own/native/cs_cpp_lib/compile.sh
@@ -5,5 +5,3 @@ rm libtest.dylib &2> /dev/null
 g++ -g -c test.cpp -o test.o                                                      # compiling a C library
 g++ -dynamiclib -undefined suppress -flat_namespace test.o -o libtest.dylib     # generate a dynamic library from our object file
 dotnet build                                                                    # compiling our C# application
-
-echo "to run, enter: dotnet run"
